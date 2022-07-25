2d1542c07096416dda48462792c9588d2154d6a5
raedaw <raedawlaw@gmail.com>
2022-07-25 10:04:06 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ const {
   handleCustomErrors,
   handleArticle404,
 } = require("./error-handling");
+const cors = require("cors");
+
+app.use(cors());
 app.use(express.json());
 
 app.get("/api", getApi);
